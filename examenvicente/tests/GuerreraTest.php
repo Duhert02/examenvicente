2c0a343b9ea1bd24875b702fdb4452ce17949f79
Duhert02 <81563377+Duhert02@users.noreply.github.com>
2022-02-02 11:49:08 +0100

    Update GuerreraTest.php

--- examenvicente/tests/GuerreraTest.php
+++ examenvicente/tests/GuerreraTest.php
@@ -1,31 +1,21 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class TestGuerrera extends TestCase{
+class TestGuerra extends TestCase {
 
-    private $op;
+    private $warrior;
 
     public function setUp():void{
-        $this->op = new Guerrera();
+        $this->warrior =  new Guerrera("Astrid",10,"viva");
     }
 
     public function testHerida(){
-        $this->assertEquals(20,$this->op->herida(30,10));
+        $this->assertEquals("muerta",$this->warrior->suspension(0));
     }
 
     public function testPocion(){
-        $this->assertEquals(20,$this->op->pocion(10,10));
+        $this->assertEquals(82,$this->warrior->pesoTotal(70,12));
     }
 
-    public function testPocionMuerta(){
-        $this->assertEquals(10,$this->op->pocion(0,10));
-    }
-
-    public function testMuerte(){
-        $this->assertEquals(-5,$this->op->herida(5,10));
-    }
-
-}
-
-
+ 
 }
