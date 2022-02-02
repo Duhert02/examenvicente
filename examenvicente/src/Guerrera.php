109fbc83e254cdfadac1530556ac5a17224810e9
Duhert02 <81563377+Duhert02@users.noreply.github.com>
2022-02-02 11:56:50 +0100

    final

--- examenvicente/src/Guerrera.php
+++ examenvicente/src/Guerrera.php
@@ -5,27 +5,21 @@ class Guerrera
     public $vida; #Valor de la vida que posee
     public $estado; #La guerrera estará 'viva' o 'muerta', dependiendo del valor de su vida. >0 = viva; <=0 = muerta
 
-    public function __construct($a1,$a2,$a3)
-    {
-        $this->nombre=$a1;
-        $this->vida=$a2;
-        $this->estado=$a3;
+    public function __construct(){}
+
+    
+    public function herida($vida,$estado){
+        $vida-=10;
+        if ($vida <= 0) {
+            $estado =='dead';
+        }
     }
 
-    public function herida($a2 , $a3){
-        $this->vida-=10;
-        if ($this->vida <= 0) {
-            $this->estado='muerta';
+    public function pocion($vida,$estado){
+        $vida+=10;
+        if ($vida > 0 && $estado == 'dead'){
+            $estado = 'alive';
         }
-
-    }
-
-    public function pocion(){
-        $this->vida+=10;
-        if ($this->vida > 0 && $this->estado == 'muerta'){
-            $this->estado = 'viva';
-        }
-
     }
 }
 ?>
